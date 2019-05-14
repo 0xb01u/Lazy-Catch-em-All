607509b0a84ba35bc95d0a9a889455d8a229d0b4
Bolu <boluelbot@gmail.com>
2019-05-14 12:14:55 +0200

    Fixed comments

--- Lazy catch'em all.asm
+++ Lazy catch'em all.asm
@@ -1,38 +1,38 @@
-ld hl, $D321		# Loads the address of the 2nd item's quantity.
-ld a, (hli)		# Loads the quantity of the 2nd item, used as loop control (should be -19).
-ld bc, $0164		# Loads in b the ID of the Pokémon to give to the player;
-			# Loads in c the level of that Pokémon (100).
+ld hl, $D321		; Loads the address of the 2nd item's quantity.
+ld a, (hli)		; Loads the quantity of the 2nd item, used as loop control (should be -19).
+ld bc, $0164		; Loads in b the ID of the Pokémon to give to the player;
+			; Loads in c the level of that Pokémon (100).
 push bc
-push af			# Stores a, b and c on the stack.
-ld b, b			# Useless instruction to make item placement easier.
-call $3E48		# Calls the 'givePokemonToPlayer' function.
-ld b, b			# Useless instruction to make item placement easier.
+push af			; Stores a, b and c on the stack.
+ld b, b			; Useless instruction to make item placement easier.
+call $3E48		; Calls the 'givePokemonToPlayer' function.
+ld b, b			; Useless instruction to make item placement easier.
 pop af
-pop bc			# Restores a, b and c.
-ld d, d			# Useless instruction to make item placement easier.
-inc a			# Increases loop counter.
-inc b			# Loads next Pokémon's ID.
-jr nz, F5		# Jumps to 5 if a != 0
+pop bc			; Restores a, b and c.
+ld d, d			; Useless instruction to make item placement easier.
+inc a			; Increases loop counter.
+inc b			; Loads next Pokémon's ID.
+jr nz, F5		; Jumps to 5 if a != 0
 ld a, b
-ld hl $D328		# Loads the address of the 5th item
-ld (hli), a		# Stores the ID of the next Pokémon given to the player on next execution
+ld hl $D328		; Loads the address of the 5th item
+ld (hli), a		; Stores the ID of the next Pokémon given to the player on next execution
 ld a, 64
 ld (hli), a
 ret
 
-# Program as hex string, and item bag:
-# 31 31		Thunderstone		x33
-# D3 2A		TM11			x42
-# 01 64		Master Ball		x100
-# 01 C5		Master Ball		x197
-# F5 40		TM45			x64
-# CD 48		TM05			x72
-# 3E 40		Lemonade		x64
-# F1 C1		Elixer			x04
-# 52 04		Fresh Water		x32
-# 3C 20		Fresh Water		x32
-# F5 78		TM42			x120
-# 21 28		Thunderstone		x04
-# D3 22		TM11			x34
-# 3E 64		Lemonade		x100
-# 22 C9		Water Stone		x201
+; Program as hex string, and item bag:
+; 31 31		Thunderstone		x33
+; D3 2A		TM11			x42
+; 01 64		Master Ball		x100
+; 01 C5		Master Ball		x197
+; F5 40		TM45			x64
+; CD 48		TM05			x72
+; 3E 40		Lemonade		x64
+; F1 C1		Elixer			x04
+; 52 04		Fresh Water		x32
+; 3C 20		Fresh Water		x32
+; F5 78		TM42			x120
+; 21 28		Thunderstone		x04
+; D3 22		TM11			x34
+; 3E 64		Lemonade		x100
+; 22 C9		Water Stone		x201
